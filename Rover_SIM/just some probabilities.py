b1c6613e23cff58439959781bf7e7b219d7e7149
Daveed-droid <d.canosaybarra@gmail.com>
2020-03-05 12:12:53 +0100

    Update just some probabilities.py

--- Rover_SIM/just some probabilities.py
+++ Rover_SIM/just some probabilities.py
@@ -3,25 +3,19 @@ from matplotlib import pyplot as plt
 from random import *
 import numpy as np
 lst=[]
+
 n=6
+array=np.zeros(n*6)
+x=range(1, n*6+1)
 for j in range(100000):
     a=0
     for i in range(n):
         a=a+randint(1,6)
-        
-    lst.append(a)
-lst.sort()
-tab=[0]*(6*n*n) #<-
-for k in range(len(lst)):
-    #print(lst[k])
-    tab[lst[k]]=tab[lst[k]]+1
 
-array=np.array(tab)
-array=np.trim_zeros(array)
-x=[]
-for q in range(n,6*n+1):
-    x.append(q)
-print(len(x))
+        
+    array[a-1]=array[a-1]+1
+
+
 print(len(array))
 plt.plot(x,array)
 plt.show()
