78108082aa1fb837f8fb376fc605feaea3db037f
Daveed-droid <d.canosaybarra@gmail.com>
2020-03-05 12:16:46 +0100

    Update just some probabilities_2.py

--- Rover_SIM/just some probabilities_2.py
+++ Rover_SIM/just some probabilities_2.py
@@ -7,8 +7,10 @@ lst=[]
 n=256
 array=np.zeros(n*6)
 x=range(1, n*6+1)
-for j in range(100000):
+amount_of_tries=100000
+for j in range(amount_of_tries):
     a=0
+    print(str((j/amount_of_tries)*100)+" % Done")
     for i in range(n):
         a=a+randint(1,6)
 
