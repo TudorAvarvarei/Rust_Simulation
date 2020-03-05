995eb9dd78b8ae8e6f1c3b168b6676a6acef4a6a
Daveed-droid <d.canosaybarra@gmail.com>
2020-03-05 12:15:18 +0100

    Create just some probabilities_2.py

---
+++ Rover_SIM/just some probabilities_2.py
@@ -0,0 +1,23 @@
+from math import *
+from matplotlib import pyplot as plt
+from random import *
+import numpy as np
+lst=[]
+
+n=256
+array=np.zeros(n*6)
+x=range(1, n*6+1)
+for j in range(100000):
+    a=0
+    for i in range(n):
+        a=a+randint(1,6)
+
+        
+    array[a-1]=array[a-1]+1
+
+
+print(len(array))
+plt.plot(x,array)
+plt.show()
+
+    
