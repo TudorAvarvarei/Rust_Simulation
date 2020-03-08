16416b19cd105f59979659dd9d465b53310f99f8
DukeOTB <61602999+DukeOTB@users.noreply.github.com>
2020-03-08 17:41:04 +0100

    Update just some probabilities_2.py

--- Rover_SIM/just some probabilities_2.py
+++ Rover_SIM/just some probabilities_2.py
@@ -7,7 +7,7 @@ lst=[]
 n=256
 array=np.zeros(n*6)
 x=range(1, n*6+1)
-amount_of_tries=100000
+amount_of_tries=1000000
 for j in range(amount_of_tries):
     a=0
     print(str((j/amount_of_tries)*100)+" % Done")
