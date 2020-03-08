9a188d94ee9054c5b155db6c725c39b79c562ec2
DukeOTB <61602999+DukeOTB@users.noreply.github.com>
2020-03-08 22:39:19 +0100

    Update just some probabilities_2.py

--- Rover_SIM/just some probabilities_2.py
+++ Rover_SIM/just some probabilities_2.py
@@ -4,10 +4,11 @@ from random import *
 import numpy as np
 lst=[]
 
-n=256
+n=150
 array=np.zeros(n*6)
+#x=range(n, n*6+1)
 x=range(1, n*6+1)
-amount_of_tries=1000000
+amount_of_tries=100000
 for j in range(amount_of_tries):
     a=0
     print(str((j/amount_of_tries)*100)+" % Done")
@@ -16,9 +17,24 @@ for j in range(amount_of_tries):
 
         
     array[a-1]=array[a-1]+1
+#array=np.trim_zeros(array)
+axis=[]
+for number in range(n,n*6+1):
+    axis.append(number)
+    
+#plt.axis([n-5, n*6+5, 0, 0.015])
+#plt.ylim(0, max(array/amount_of_tries)*1.5)
+#plt.plot(x,array/amount_of_tries)
+#plt.show()
 
+print(axis)
+print(x)
+print(len(x))
+print(array)
+print(len(array))
+plt.bar(x,array/amount_of_tries, align='center',alpha=0.5)
 
-plt.axis([750, 1000, 0, 0.015])
-plt.plot(x,array/amount_of_tries)
+plt.xticks(x,axis)
+plt.xlim(n,n*6+1)
 plt.show()
 
